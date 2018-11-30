e0954eccebefe6b49717eac75a7ad7cc2bd4a4ba
Corina Pelichowski <corina@danitt.com>
2018-11-30 18:29:23 +1000

    Tutorial = Complete

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 16-JS-CSS-Text-Shadow-Mouse-Move-Effect
+PLaying around with different colours and shadows based on the movements of the mouse.
+
+Thanks to Wes Bos for this tutorial.
---
+++ js and decimals.js
@@ -0,0 +1,9 @@
+const total = 0.1 + 0.2;
+if (total === 0.3) {
+  console.log('you have 30 cents');
+} else {
+  console.log("you DON'T have 30 cents");
+}
+
+
+console.log('the total is: ', total);
